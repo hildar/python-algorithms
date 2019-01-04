8ea925ae9644e00522cbe8d5bf1b8073e85d2e72
hildar <shousediy@gmail.com>
2019-01-04 18:39:32 +0900

    upd 1

--- Lesson_3/task_1.py
+++ Lesson_3/task_1.py
@@ -4,28 +4,15 @@
 # SIZE = 98
 MIN_ITEM = 2
 MAX_ITEM = 99
-# array = [_ for _ in range(MIN_ITEM,MAX_ITEM)]  # по сути массив не обязательно создавать
-int_2, int_3, int_4, int_5, int_6, int_7, int_8, int_9, = 0, 0, 0, 0, 0, 0, 0, 0
+MIN_MULT = 2  # multiplicity of numbers - кратность числа
+MAX_MULT = 9
+array = [0 for _ in range(MIN_MULT, MAX_MULT + 1)]
 
-for i in range(MIN_ITEM,MAX_ITEM):
-    if i % 2 == 0:
-        int_2 += 1
-    if i % 3 == 0:
-        int_3 += 1
-    if i % 4 == 0:
-        int_4 += 1
-    if i % 5 == 0:
-        int_5 += 1
-    if i % 6 == 0:
-        int_6 += 1
-    if i % 7 == 0:
-        int_7 += 1
-    if i % 8 == 0:
-        int_8 += 1
-    if i % 9 == 0:
-        int_9 += 1
+for i in range(MIN_ITEM, MAX_ITEM):
+    for j in range(MIN_MULT, MAX_MULT + 1):
+        if i % j == 0:
+            array[j - MIN_MULT] += 1
 
-
-print(f'В диапазоне чисел от {MIN_ITEM} до {MAX_ITEM} кратны любому от 2 до 9:\n'
-      f'2 = {int_2}\n3 = {int_3}\n4 = {int_4}\n5 = {int_5}\n6 = {int_6}\n'
-      f'7 = {int_7}\n8 = {int_8}\n9 = {int_9}')
+print(f'В диапазоне чисел от {MIN_ITEM} до {MAX_ITEM}, кратные любому из чисел от 2 до 9:')
+for i, item in enumerate(array):
+    print(f'{i + MIN_MULT} = {item}')
