2a49a081fbd3b4a4599abd982234bd9358a4bf33
hildar <shousediy@gmail.com>
2019-01-04 17:29:55 +0900

    4 update

--- Lesson_3/task_4.py
+++ Lesson_3/task_4.py
@@ -2,14 +2,14 @@
 
 import random
 
-SIZE = 10
-MIN_ITEM = 0
-MAX_ITEM = 8
+SIZE = 20
+MIN_ITEM = -10
+MAX_ITEM = 10
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
-array2 = [0 for _ in range(MIN_ITEM, MAX_ITEM + 1)]  # создаем для использоавания индексов
+array2 = [0 for _ in range(MIN_ITEM, MAX_ITEM + 1)]  # создаем для использования индексов
 
 for i in array:
-    array2[i] += 1
+    array2[i - MIN_ITEM] += 1
 
 max_num = 0
 spam_max = 0
@@ -20,4 +20,4 @@ for i, item in enumerate(array2):
         max_num = i
 
 print(f'Массив array: {array}')
-print(f'Чаще всего в массиве array встречается число: {max_num}')
+print(f'Чаще всего в массиве array встречается число: {max_num + MIN_ITEM}')
