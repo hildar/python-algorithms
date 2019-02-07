cdd6148db4c6bdb62a7a64cb6e4f52cb3919ca8e
hildar <shousediy@gmail.com>
2019-02-07 12:38:27 +0900

    Выполнил - # 3. Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом. Найдите в массиве медиану.

--- Lesson_7/task_3.py
+++ Lesson_7/task_3.py
@@ -6,8 +6,13 @@
 import random
 
 
+# Функция разбиения массива и поиска опорного индекса
 def partition(array, first, last):
+    # Добавим элемент случайности выбора для того чтобы время поиска было в среднем O(n * log(n))
+    q = random.randrange(first, last + 1)
+    array[q], array[last] = array[last], array[q]
 
+    # Для удобства за опорный элемент принимаем последний
     pivot = array[last]
     i = first - 1
 
@@ -18,50 +23,38 @@ def partition(array, first, last):
 
     array[i + 1], array[last] = array[last], array[i + 1]
 
-    # q = random.randint(first, last)
-    # pivot = array[q]
-    # i, j = first, last
-    #
-    # while i <= j:
-    #     while array[i] < pivot:
-    #         i += 1
-    #     while array[j] > pivot:
-    #         j -= 1
-    #
-    #     if i <= j:
-    #         array[i], array[j] = array[j], array[i]
-    #         i += 1
-    #         j -= 1
-
+    # возвращаем индекс опорного элемента
     return i + 1
 
 
+# Функция выбора (поиск медианы - частный случай)
 def select(array, first, last, i):
     # base case
-    if first >= last:
+    if first == last:
         return array[first]
 
     q = partition(array, first, last)
-    k = q - first + 1   # Плюс один опорный элемент
+    k = q - first + 1   # Множество левой части разбиения плюс один опорный элемент
 
     if i == k:
         return array[q]  # Ответом является опорное значение
     elif i < k:
-
-        return select(array, first, q - 1, i)
+        return select(array, first, q - 1, i)  # Ищем в левой части массива
     else:
-        return select(array, q + 1, last, i - k)
+        return select(array, q + 1, last, i - k)  # Иначе в правой части массива
 
 
-m = 4
+m = int(input('Введите натуральное целое число m. Размерность массива будет согласно формуле (2 * m + 1) : '))
 
-# array = [random.randrange(0, 100) for i in range(2 * m + 1)]
-array = [i * i * 2 for i in range(2 * m + 1)]
-# array = [2, 8, 7, 1, 3, 5, 6, 2, 4]
-print(array)
-random.shuffle(array)
+array = [random.randrange(0, 100) for i in range(2 * m + 1)]
 print(array)
 
-median = select(array, 0, len(array) - 1, (len(array) - 1) / 2)
+# Т.к. медиана по математическому порядку считается с одного то и индекс ищем на один больше
+i = (len(array) + 1) / 2
+
+median = select(array, 0, len(array) - 1, i)
+print(f'median = {median}')
+
+# for check:
 # array.sort()
-print(median)
+# print(array)
